bd69c42b9245dee63b4356b9753daa27e7d128ab
jeroke7 <jessecakelsey@gmail.com>
2023-11-06 12:45:10 -0500

    commit

--- js/rps.js
+++ js/rps.js
@@ -2,12 +2,16 @@
 const rockButton = document.getElementById('rock')
 const paperButton = document.getElementById('paper')
 const scissorsButton = document.getElementById('scissors')
+// const yourScore = document.getElementById('.user-score')
+// const computerScore = document.getElementById('.computer-score')
 const result = document.querySelector('.result')
 const userImg = document.querySelector('.user-img')
 const compImg = document.querySelector('.comp-img')
 
 const choices = ['rock', 'paper', 'scissors']
 let comp
+let userScore = 0
+let computerScore = 0
 // recheck
 
 // console.log(userImg)
@@ -24,30 +28,56 @@ let comp
 rockButton.addEventListener('click', () => {
     userImg.src = "../img/rps" + rockButton.id + ".png"
     compChoice()
-    // result.style.color = "red"
-    // change object styles using .style followed by 
+    winLose(rockButton.id)
+    // userScore = userScore + 1
+    // result.innerText = userScore.toString()
 })
 paperButton.addEventListener('click', () => {
     userImg.src = "../img/rps" + paperButton.id + ".png"
     compChoice()
+    winLose(paperButton.id)
+    // userScore = userScore + 1
+    // result.innerText = userScore.toString()
 })
 scissorsButton.addEventListener('click', () => {
     userImg.src = "../img/rps" + scissorsButton.id + ".png"
     compChoice()
+    winLose(scissorsButton.id)
+    // userScore = userScore + 1
+    // result.innerText = userScore.toString()
 })
 
 function compChoice () {
     const randNum = Math.floor(Math.random()*3)
-    const comp = choices[randNum]
-    console.log(choices)
+    comp = choices[randNum]
+    console.log(comp)
     compImg.src = "../img/rps" + comp + ".png"
 }
 
-// function winLose(userChoice) {
-//     console.log(comp, userChoice)
-// }
+function winLose(userChoice) {
+    if (userChoice == "rock" && comp == "paper"){
+        result.innerText = 'Computer Wins :('
+    } else if (userChoice == "scissors" && comp == "rock"){
+        result.innerText = 'Computer Wins :('
+    } else if (userChoice == "paper" && comp == "scissors"){
+        result.innerText = 'Computer Wins :('
+    } else if (comp == "rock" && userChoice == "scissors"){
+        result.innerText = 'You Win :)'
+    } else if (comp == "rock" && userChoice == "paper"){
+        result.innerText = 'You Win :)'
+    } else if (comp == "paper" && userChoice == "scissors"){
+        result.innerText = 'You Win :)'
+    } else if (comp == "scissors" && userChoice == "rock"){
+        result.innerText = 'You Win :)'
+    } else if (comp == "rock" && userChoice == "rock"){
+        result.innerText = 'Tie! :0'
+    } else if (comp == "paper" && userChoice == "paper"){
+        result.innerText = 'Tie! :0'
+    } else if (comp == "scissors" && userChoice == "scissors"){
+        result.innerText = 'Tie! :0'
+    }
+}
 
-// winLose('rock')
 
 
 
