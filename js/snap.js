1e7020d3eb6fb6c4b4d6a4aaa0dcc5cd985b34fc
jeroke7 <jessecakelsey@gmail.com>
2024-02-29 12:43:50 -0500

    commit

--- js/snap.js
+++ js/snap.js
@@ -1,24 +1,8 @@
-const upAngleList = document.querySelectorAll('.fa-angle-up')
-const downAngleList = document.querySelectorAll('.fa-angle-down')
-const dropItmsList = document.querySelectorAll('dropitms')
+const openBtn = document.getElementById('#open')
+const close = document.getElementById('#close')
+const dropItemList = document.querySelector('.dropitms')
+const menu = document.querySelector('.menu')
 
-
-
-//open
-upAngleList.forEach(up => {
-    up.addEventListener('click', () => {
-        let Angle = parseInt(up.id)
-        upAngleList[angle].style.display='none'
-        downAngleList[angle].style.display='flex'
-        dropItmsList[angle].style.display='flex'
-    })
-})
-//closed
-downAngleList.forEach(down => {
-    down.addEventListener('click', () => {
-        let Angle = parseInt(down.id)
-        upAngleList[angle].style.display='flex'
-        downAngleList[angle].style.display='none'
-        dropItmsList[angle].style.display='none'
-    })
+openBtn.addEventListener('click', () => {
+    console.log('clicked')
 })
