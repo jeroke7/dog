37a87f1ced95df6e3a284fd1bc5d2c9db383c3e3
jeroke7 <jessecakelsey@gmail.com>
2024-03-04 12:45:20 -0500

    cpmmit

--- js/snap.js
+++ js/snap.js
@@ -1,8 +1,19 @@
-const openBtn = document.getElementById('#open')
-const close = document.getElementById('#close')
+const openBtn = document.getElementById('open')
+const closeBtn = document.getElementById('close')
 const dropItemList = document.querySelector('.dropitms')
 const menu = document.querySelector('.menu')
+const fade = document.querySelector('.fade')
 
 openBtn.addEventListener('click', () => {
-    console.log('clicked')
+    menu.style.display = 'flex'
+    openBtn.style.display = 'none'
+    closeBtn.style.display = 'flex'
+    fade.style.display = 'flex'
+})
+
+closeBtn.addEventListener('click', () => {
+    menu.style.display = 'none'
+    openBtn.style.display = 'block'
+    fade.style.display = 'none'
+
 })
